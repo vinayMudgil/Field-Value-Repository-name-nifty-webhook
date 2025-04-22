82e7c0d7d9adcdac6facbf1f078697bb7927cee9
vinayMudgil <vsvinay32@gmail.com>
2025-04-22 21:39:15 +0530

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, request
 import json
 
@@ -7,14 +6,17 @@ app = Flask(__name__)
 @app.route("/", methods=["POST"])
 def webhook():
     data = json.loads(request.data)
-    
+
     # Extract custom fields from TradingView if sent
-    signal = data.get("action", "Signal")
+    signal = data.get("action", "signal")
     strike = data.get("strike", "Unknown")
-    
-    print(f"\n📢 SIGNAL RECEIVED:")
+
+    print(f"\n\n SIGNAL RECEIVED:")
     print(f"Action : {signal}")
     print(f"Strike : {strike}")
-    print(f"Time   : {data.get('time', 'Unknown')}\n")
+    print(f"Time   : {data.get('time', 'Unknown')}\n\n")
 
     return "Signal received", 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
